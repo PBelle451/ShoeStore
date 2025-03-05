d7e4d9dec3557cd7b00696f3b8a7189a426288a3
PBelle451 <lordbelle7@gmail.com>
2025-03-05 16:10:10 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// You can add JavaScript functionality here, such as:
+// - Image carousel for the hero section
+// - Adding items to the cart
+// - Form validation for the contact form
+console.log("Shoe store script loaded");
+
